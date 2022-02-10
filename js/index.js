8d6530f48e0c65a357c38973a35d305f6a8ca231
rajeshpullagura47 <rajeshchinna47@gmail.com>
2022-02-10 16:37:14 +0530

    duplicatefix

--- js/index.js
+++ js/index.js
@@ -1,32 +1,47 @@
 let attempts = 6;
 
-const WORD = WORDS[Math.floor(Math.random() *  WORDS.length)];
+const str1 = WORDS[Math.floor(Math.random() *  WORDS.length)];
 const el = document.querySelector("#guess");
-
-console.log("Target:", WORD);
+str12=str1.toLowerCase();
+console.log("Target:", str1);
+const tar=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+for(var i=0;i<str12.length;i++)
+    tar[str12.charCodeAt(i)-97]+=1;
 
 function registerGuess(guess) {
-    guess = guess.toUpperCase();
-    const status = [];
-    const WORD_LETTERS = WORD.split("");
-    guess.split("").forEach(function(letter, index) {
-        // TODO: handle additional letters when there are duplicates
-        let letterStatus;
-        const existsInWord = WORD_LETTERS.indexOf(letter) > -1;
-        const isInPlace = WORD_LETTERS[index] === letter;
-        if (isInPlace) {
-            letterStatus = 2;
-        } else if (existsInWord) {
-            letterStatus = 1;
-        } else {
-            letterStatus = 0;
+    gue = guess.toLowerCase();
+    const control=[0,0,0,0,0]
+    const gues=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+for(var i=0;i<gue.length;i++)
+    gues[gue.charCodeAt(i)-97]+=1
+var limit=-100;
+for(var i=0;i<26;i++){
+    if(tar[i]>0 && gues[i]>0){
+        if(tar[i]>=gues[i])
+            limit=gues[i]
+        else
+            limit=tar[i]
+        c=0
+        for(var j=0;j<5;j++){
+            if((gue[j]==String.fromCharCode(i+97) && gue[j]==str12[j])){
+                control[j]=2
+                c+=1
+            }
         }
-        status.push(letterStatus);
-    })
-    printGuess(guess, status);
-    return status;
+        k=0
+        while(c<limit && k<=4){
+            if(control[k]==0 && gue[k]==String.fromCharCode(i+97)){
+                control[k]=1
+                c+=1
+            }
+            k+=1
+        }
+    }
 }
-
+console.log(control);
+    printGuess(gue, control);
+    return control;
+}
 el.focus();
 
 el.addEventListener("blur", function(e) {
